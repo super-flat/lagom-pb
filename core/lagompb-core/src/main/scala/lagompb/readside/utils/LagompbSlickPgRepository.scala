671cfbb47e28a4c8e15242509050626331a365ec
Arsene Tochemey GANDOTE <tochemey@hotmail.com>
2020-05-30 16:35:55 +0000

    issue #32: fix LagompbSlickPgRepository

--- core/lagompb-core/src/main/scala/lagompb/readside/utils/LagompbSlickPgRepository.scala
+++ core/lagompb-core/src/main/scala/lagompb/readside/utils/LagompbSlickPgRepository.scala
@@ -10,57 +10,51 @@ import scala.concurrent.Future
 /**
  * Repository interface that will be implemented by any slick postgres readSide
  */
-trait LagompbSlickPgRepository[T] {
+abstract class LagompbSlickPgRepository[T](tableQuery: TableQuery[T], database: Database) {
 
   /**
    * Creates a new record in the database
    *
-   * @param model    the data model to persist
-   * @param database the implicit database connection
+   * @param model the data model to persist
    * @return the created data model
    */
-  def save(model: T)(implicit database: Database): Future[T]
+  def save(model: T): Future[T]
 
   /**
    * Retrieves a record from the database with the corresponding id.
    *
    * @param entityId the record unique identifier
-   * @param database the implicit database connection
    * @return the record fetched or None if it was not found
    */
-  def read(entityId: String)(implicit database: Database): Future[Option[T]]
+  def read(entityId: String): Future[Option[T]]
 
   /**
    * Retrieves all records from the database
    *
    * @return the list of records fetched
    */
-  def all()(implicit database: Database): Future[Seq[T]]
+  def all(): Future[Seq[T]]
 
   /**
    * Update the corresponding record in the database.
    *
    * @param model the data model of the record to update
-   * @param database the implicit database connection
    * @return the updated record or None if it was not found
    */
-  def update(model: T)(implicit database: Database): Future[Option[T]]
+  def update(model: T): Future[Option[T]]
 
   /**
    * Deletes a record from the database with the corresponding id.
    *
    * @param entityId the record unique identifier
-   * @param database the implicit database connection
    * @return the deleted record or None if it was not found
    */
-  def delete(entityId: String)(implicit database: Database): Future[Option[T]]
+  def delete(entityId: String): Future[Option[T]]
 
   /**
    * Creates the database table if the table does not exist.
    *
    * @return
    */
-  def createSchema()(
-      implicit database: Database
-  ): PostgresProfile.api.DBIOAction[Unit, PostgresProfile.api.NoStream, Effect.Schema]
+  def createSchema(): PostgresProfile.api.DBIOAction[Unit, PostgresProfile.api.NoStream, Effect.Schema]
 }
