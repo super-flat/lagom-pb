04e35adc1aa1255e409f8c20a57c1abedef28b15
Arsene Tochemey GANDOTE <tochemey@hotmail.com>
2020-06-14 00:56:27 +0000

    issue #66: quick update

--- core/lagompb-core/src/main/scala/lagompb/LagompbSerializer.scala
+++ core/lagompb-core/src/main/scala/lagompb/LagompbSerializer.scala
@@ -2,33 +2,63 @@ package lagompb
 
 import akka.util.ByteString
 import com.lightbend.lagom.scaladsl.api.deser.{MessageSerializer, StrictMessageSerializer}
-import com.lightbend.lagom.scaladsl.api.deser.MessageSerializer.NegotiatedSerializer
+import com.lightbend.lagom.scaladsl.api.deser.MessageSerializer.{NegotiatedDeserializer, NegotiatedSerializer}
 import com.lightbend.lagom.scaladsl.api.transport.MessageProtocol
 import scalapb.{GeneratedMessage, GeneratedMessageCompanion}
 
-class LagompbSerializer[T <: GeneratedMessage: GeneratedMessageCompanion] extends StrictMessageSerializer[T] {
+class LagompbSerializer[T <: GeneratedMessage: GeneratedMessageCompanion]
+    extends StrictMessageSerializer[T]
+    with GenericSerializers[T] {
 
-  private final val serializerJson = {
-    new NegotiatedSerializer[T, ByteString]() {
+  override def serializerForRequest: MessageSerializer.NegotiatedSerializer[T, ByteString] = serializerJson
+
+  override def deserializer(protocol: MessageProtocol): MessageSerializer.NegotiatedDeserializer[T, ByteString] =
+    deserializer
+
+  override def serializerForResponse(
+      acceptedMessageProtocols: Seq[MessageProtocol]
+  ): MessageSerializer.NegotiatedSerializer[T, ByteString] = negotiateResponse(acceptedMessageProtocols)
+}
+
+sealed trait GenericSerializers[T <: GeneratedMessage] {
+
+  def serializerJson: NegotiatedSerializer[T, ByteString] = {
+    new NegotiatedSerializer[T, ByteString] {
       override def protocol: MessageProtocol =
         MessageProtocol(Some("application/json"))
-
-      def serialize(message: T): ByteString = {
+      override def serialize(message: T): ByteString = {
         ByteString(LagompbProtosRegistry.printer.print(message))
       }
     }
   }
 
-  override def serializerForRequest: MessageSerializer.NegotiatedSerializer[T, ByteString] = serializerJson
+  def serializerProtobuf: NegotiatedSerializer[T, ByteString] = {
+    new NegotiatedSerializer[T, ByteString] {
+      override def protocol: MessageProtocol =
+        MessageProtocol(Some("application/x-protobuf"))
 
-  override def deserializer(protocol: MessageProtocol): MessageSerializer.NegotiatedDeserializer[T, ByteString] = {
+      override def serialize(message: T): ByteString = {
+        ByteString(message.toByteArray)
+      }
+    }
+  }
+
+  def deserializer(implicit T: GeneratedMessageCompanion[T]): NegotiatedDeserializer[T, ByteString] = {
     (wire: ByteString) =>
       {
         LagompbProtosRegistry.parser.fromJsonString(wire.utf8String)
       }
   }
 
-  override def serializerForResponse(
-      acceptedMessageProtocols: Seq[MessageProtocol]
-  ): MessageSerializer.NegotiatedSerializer[T, ByteString] = serializerJson
+  def negotiateResponse(acceptedMessageProtocols: Seq[MessageProtocol]): NegotiatedSerializer[T, ByteString] = {
+    acceptedMessageProtocols match {
+      case Nil => serializerJson
+      case protocols =>
+        protocols.collectFirst {
+          case MessageProtocol(Some("application/x-protobuf"), _, _) =>
+            serializerProtobuf
+          case _ => serializerJson
+        }.get
+    }
+  }
 }
