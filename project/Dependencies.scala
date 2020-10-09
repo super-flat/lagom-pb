0c4df033f7a50fcfdffee8f95f7842fcc65630e3
Scala Steward <me@scala-steward.org>
2020-10-09 17:38:57 +0200

    Update postgresql to 42.2.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val ScalaTestVersion = "3.2.2"
     val PlayJsonDerivedCodecsVersion = "7.0.0"
     val AkkaDiscoveryKubernetesApiVersion = "1.0.8"
-    val PostgresDriverVersion = "42.2.16"
+    val PostgresDriverVersion = "42.2.17"
     val AkkaManagementVersion = "1.0.8"
     val AkkaManagementClusterBootstrapVersion = "1.0.8"
     val AkkaManagementClusterHttpVersion = "1.0.8"
