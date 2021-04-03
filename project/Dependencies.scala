3e7351b48837db0d737acb19830354952a1c5474
Scala Steward <me@scala-steward.org>
2021-04-03 16:52:26 +0200

    Update compilerplugin to 0.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
     val CatsVersion = "2.5.0"
 
     val SbtProtocVersion = "1.0.0"
-    val ScalapbCompilerVersion = "0.11.0"
+    val ScalapbCompilerVersion = "0.11.1"
     val ScalapbValidationVersion = "0.3.0"
     val JavaAgentVersion = "0.1.5"
     val CrossScalaForPlugin = Seq(Scala212)
