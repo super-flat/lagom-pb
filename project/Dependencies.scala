9024b15b1ffba26781961c46ff388136f77bd087
Scala Steward <me@scala-steward.org>
2021-11-30 06:06:25 +0100

    Update h2 to 2.0.202

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val ScalaMockVersion = "5.1.0"
     val SilencerVersion = "1.7.5"
     val AkkaGrpcVersion = "1.0.3"
-    val H2Version = "1.4.200"
+    val H2Version = "2.0.202"
     val ScalapbJson4sVersion = "0.12.0"
     val PlayGrpcVersion = "0.9.1"
     val ReflectionsVersion = "0.9.12"
